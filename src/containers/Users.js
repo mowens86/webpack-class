c366dd4fb608dc6e40467592322d00a9797569fa
Mike Owens <mocoding86@gmail.com>
2021-01-30 15:31:59 -0800

    class finished

---
+++ src/containers/Users.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Users extends Component {
+    render () {
+        return (
+            <div>
+                <h1>The User</h1>
+                <p>I am gonna finish this course and kick ass in React.</p>
+            </div>
+        )
+    }
+}
+
+export default Users;
